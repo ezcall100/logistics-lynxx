1903eda9906d4d3bf81077b888ff8d8748627cd6
ezcall100 <ezcallnet.mo@gmail.com>
2025-08-22 23:37:05 -0700

    feat: integrate HashRouter for improved routing in the main application

--- logistics-lynx/src/main.tsx
+++ logistics-lynx/src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css"; // ok if missing; remove this line if you don't have it
 
 const root = document.getElementById("root")!;
 createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
