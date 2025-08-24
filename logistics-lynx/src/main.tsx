23a25649d1a604dee865d599e4fa9f16b3616cce
ezcall100 <ezcallnet.mo@gmail.com>
2025-08-23 19:48:34 -0700

    refactor: remove AuthProvider from main application to simplify rendering structure

--- logistics-lynx/src/main.tsx
+++ logistics-lynx/src/main.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
-import { AuthProvider } from './context/auth'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </HashRouter>
   </React.StrictMode>,
 )
